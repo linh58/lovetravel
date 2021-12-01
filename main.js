24450cdb02d87aac3a4dac0671cc208ee69d9d89
Linh <lemailinh22@gmail.com>
2021-12-02 01:27:43 +0700

    update event slideshow

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+var $ = document.querySelector.bind(document)
+var $$ = document.querySelectorAll.bind(document)
 /* PC Menu */
-var navbarButton = document.querySelector('.header__navbar-icon')
-var smallSection = document.querySelector('.small__section')
-var closeButton = document.querySelector('.close-icon')
+var navbarButton = $('.header__navbar-icon')
+var smallSection = $('.small__section')
+var closeButton = $('.close-icon')
 
 function showSection() {
     smallSection.classList.add('small__section-show')
@@ -16,8 +18,8 @@ closeButton.addEventListener('click', hiddenSection)
 
 
 /* Mobile navbar */
-var navBarMobile = document.querySelector('.navbar-on-mobile')
-var mobileCloseButton = document.querySelector('.mobile__close-btn-img')
+var navBarMobile = $('.navbar-on-mobile')
+var mobileCloseButton = $('.mobile__close-btn-img')
 
 function showMobileNav () {
     var width = window.innerWidth
@@ -36,3 +38,18 @@ function hiddenMobileNav () {
 navbarButton.addEventListener('click', showMobileNav)
 mobileCloseButton.addEventListener('click', hiddenMobileNav)
 
+/* Slideshow */
+var slideButtons = $$('.light-bar')
+
+slideButtons.forEach(function(slideButton, index) {
+    var slidePhotos = $$('.slideshow-photo')
+    slideButton.onclick = function() {
+        var slidePhoto = slidePhotos[index]
+
+        $('.light-bar.selected').classList.remove('selected')
+        $('.slideshow-photo.slideshow-active').classList.remove('slideshow-active')
+
+        this.classList.add('selected')
+        slidePhoto.classList.add('slideshow-active')
+    }
+})
