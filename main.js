041dc93e88581f3deff821097602bf74b5fbde14
Linh <lemailinh22@gmail.com>
2021-12-07 14:46:30 +0700

    update js file

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var $ = document.querySelector.bind(document)
 var $$ = document.querySelectorAll.bind(document)
+
 /* PC Menu */
 var navbarButton = $('.header__navbar-icon')
 var smallSection = $('.small__section')
@@ -18,38 +19,46 @@ closeButton.addEventListener('click', hiddenSection)
 
 
 /* Mobile navbar */
-var navBarMobile = $('.navbar-on-mobile')
-var mobileCloseButton = $('.mobile__close-btn-img')
-
-function showMobileNav () {
-    var width = window.innerWidth
-    document.documentElement.clientWidth
-    document.body.clientWidth;
-
-    if(width <= 1230) {
-        navBarMobile.classList.add('show-navbar-on-mobile')
+function mobileNavbar() {
+    var navBarMobile = $('.navbar-on-mobile')
+    var mobileCloseButton = $('.mobile__close-btn-img')
+    
+    function showMobileNav () {
+        var width = window.innerWidth
+        document.documentElement.clientWidth
+        document.body.clientWidth;
+    
+        if(width <= 1230) {
+            navBarMobile.classList.add('show-navbar-on-mobile')
+        }
     }
+    
+    function hiddenMobileNav () {
+        navBarMobile.classList.remove('show-navbar-on-mobile')
+    }
+    
+    navbarButton.addEventListener('click', showMobileNav)
+    mobileCloseButton.addEventListener('click', hiddenMobileNav)
 }
 
-function hiddenMobileNav () {
-    navBarMobile.classList.remove('show-navbar-on-mobile')
-}
-
-navbarButton.addEventListener('click', showMobileNav)
-mobileCloseButton.addEventListener('click', hiddenMobileNav)
+mobileNavbar()
 
 /* Slideshow */
-var slideButtons = $$('.light-bar')
+function slideShow() {
+    var slideButtons = $$('.light-bar')
+    
+    slideButtons.forEach(function(slideButton, index) {
+        var slidePhotos = $$('.slideshow-photo')
+        slideButton.onclick = function() {
+            var slidePhoto = slidePhotos[index]
+    
+            $('.light-bar.selected').classList.remove('selected')
+            $('.slideshow-photo.slideshow-active').classList.remove('slideshow-active')
+    
+            this.classList.add('selected')
+            slidePhoto.classList.add('slideshow-active')
+        }
+    })
+}
 
-slideButtons.forEach(function(slideButton, index) {
-    var slidePhotos = $$('.slideshow-photo')
-    slideButton.onclick = function() {
-        var slidePhoto = slidePhotos[index]
-
-        $('.light-bar.selected').classList.remove('selected')
-        $('.slideshow-photo.slideshow-active').classList.remove('slideshow-active')
-
-        this.classList.add('selected')
-        slidePhoto.classList.add('slideshow-active')
-    }
-})
+slideShow()
